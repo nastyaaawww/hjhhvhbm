29163634806972b01e8d54c338cabbc0c162a967
nastyaaawww <161913300+nastyaaawww@users.noreply.github.com>
2024-03-16 17:08:19 +1200

    script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-alert(5) 
+
 let block=document.querySelector('.block')
 let create_card_btn=document.querySelector('body')
 
 create_card_btn.addEventListener('click',function(e){
-    block.style.transform=`translate(${e.pageX-25}px,${e.pageY-25}px)`
+    block.style.transform=`translate(${e.pageX-25}px,${e.pageY-600}px)`
     block.style.opacity=1
     anime({
         targets:'.block',
